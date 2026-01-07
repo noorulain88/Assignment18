36ddf0757b143a02094c5a005f2bd68ee8534380
noorulain88 <noorulainain88@gmail.com>
2026-01-07 22:47:45 +0500

    Assignment 18

--- program.js
+++ program.js
@@ -30,6 +30,72 @@ function getPassword(){
     }
 
 }
+// 
+function getPercent(){
+    var obtainMarks=document.getElementById("obtain").value
+    var totalMarks=document.getElementById("total").value
+    var calculate=obtainMarks/totalMarks
+    var value=calculate*100
+    var roundOff=Math.floor(value)
+  let grade;
+  switch(true){
+    case (roundOff>=80):
+        grade="A+";
+        break;
+            case (roundOff>=70):
+        grade="B";
+        break;
+            case (roundOff>=60):
+        grade="C";
+        break;
+        default:
+            grade="fail"
+
+  }
+    document.getElementById("user-marks").innerHTML="your percentage is "+grade
+}
+// part 02
+// program 01
+var student={
+    name:"Noor",
+    age:"17",
+    course:"WEb development",
+}
+console.log("The user name is  ",student.name)
+console.log("The user age is  ",student.age)
+console.log("The user course is ",student.course)
+
+// progrma 02
+var car={
+    model:"Civic",
+    brand:"Honda",
+    year:2025
+
+}
+car.year=2026
+console.log("the update year is",car.year)
+
+// program 03
+var user={
+    Name:"john",
+    Email:true,
+}
+console.log("email is loggdin ",user.Email)
+// program 04
+var product={
+    productName:"Laptop",
+    prize:"70000",
+    inStock:true
+
+}
+product.prize=80000
+console.log("the update prize is",product.prize)
+// progrma 05
+var Name={
+    firstName:"W.H",
+    lastName:"Daivs",
+}
+      console.log("user full name is ",Name.firstName+ Name.lastName)
 
 
 
